337d5c99b56a8328dd81ceb0a35e0173156fda56
Ichoku Chinonso <greatchinonso7@gmail.com>
2020-11-07 11:33:49 +0100

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,70 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div style={{ overflow: 'hidden' }}>
       <Head>
-        <title>Create Next App</title>
+        <title>Trav4College</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className={styles.container}>
+          <div className={styles.header}>
+            <div className={styles.logo}>
+              <Image
+                src='/assets/images/logo.svg'
+                width={50}
+                height={50}
+                alt="Logo"
+              />
+              <span className={styles.text}>Trav4College</span>
+            </div>
+            <div className={styles.navbar}>
+              <ul className={styles.navlist}>
+                <li className={styles.navItem}>College Application</li>
+                <li className={styles.navItem}>Scholarships</li>
+                <li className={styles.navItem}>College Savings</li>
+                <li className={styles.navItem}>International Exams</li>
+                <li className={styles.navItem}>Educational Partners</li>
+              </ul>
+            </div>
+            <div className={styles.auth}>
+              <a className={styles.login}>Login</a>
+              <button className={styles.register}>Get Started</button>
+            </div>
+          </div>
+        </div>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <div style={{ backgroundColor: '#F2F9FF' }}>
+          <div className={styles.container} style={{ maxHeight: "150%" }}>
+            <div style={{ maxWidth: '50%' }}>
+              <h1 style={{ fontSize: 65, marginTop: 200 }}>Your College Journey <br /> starts right here.</h1>
+              <p style={{ fontSize: 24, lineHeight: 1.3, fontWeight: 300, color: '#667586' }}>Trav4College offers robust platform that includes rich details <br /> on admissions, student profiles, cost of study, graduate <br /> outcomes and more</p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+              <div style={{ marginTop: 50 }}>
+                <button className={styles.signup}>Sign up</button>
+                <button className={styles.download}>Download App</button>
+              </div>
+            </div>
+            <div style={{ maxWidth: '50%', margin: 'auto' }}>
+              <div style={{ position: 'absolute', top: 150, right: 360 }}>
+                <img src="/assets/images/student3.jpg" width="320px" height="600px" />
+              </div>
+              <div style={{ flexDirection: 'row', position: 'relative', left: 700, bottom: 570, maxWidth: 300 }}>
+                <img src="/assets/images/student1.jpg" width="280px" height="280px" style={{ marginBottom: 35 }} />
+                <img src="/assets/images/student2.jpg" width="280px" height="280px" />
+              </div>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+            </div>
+          </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
